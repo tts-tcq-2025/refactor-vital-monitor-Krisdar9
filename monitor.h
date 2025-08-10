d272b680f13ef05a05f8be847e9fda741415951d
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-08-10 18:48:00 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -12,9 +12,13 @@ enum class VitalStatus {
     PulseOutOfRange,
     Spo2OutOfRange
 };
-
+VitalStatus checkTemperature(float temp);
+VitalStatus checkPulse(float pulse);
+VitalStatus checkSpo2(float spo2);
 VitalStatus evaluateVitals(const VitalSigns& vitals);
+
 void printAlert(VitalStatus status);
 void blinkIndicator();
 int vitalsOk(float temperature, float pulseRate, float spo2);
+
 
