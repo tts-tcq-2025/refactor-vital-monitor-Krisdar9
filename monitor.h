c7eb7c21bbeeba0a57f3301bb92af7afe14852b4
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-08-10 18:28:55 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,20 @@
 #pragma once
 
+struct VitalSigns {
+    float temperature;
+    float pulseRate;
+    float spo2;
+};
+
+enum class VitalStatus {
+    OK,
+    TemperatureOutOfRange,
+    PulseOutOfRange,
+    Spo2OutOfRange
+};
+
+VitalStatus evaluateVitals(const VitalSigns& vitals);
+void printAlert(VitalStatus status);
+void blinkIndicator();
 int vitalsOk(float temperature, float pulseRate, float spo2);
+
