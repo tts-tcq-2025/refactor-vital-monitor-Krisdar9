3a8b5f68a1f4fca273496290d93a372c552bd8fd
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-20 13:25:28 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,6 @@
 #include "monitor.h"
 #include <assert.h>
 #include <iostream>
-using namespace std;
 
 void testCheckVital() {
     assert(checkVital(50.0f, 40.0f, 60.0f, 0.1f, VitalStatus::PulseOutOfRange,
@@ -13,21 +12,17 @@ void testCheckVital() {
 }
 
 void testWarnings() {
-    cout << "Testing temperature warnings:\n";
     printTemperatureWarning(95.0f, 95.0f, 102.0f, 102.0f * 0.015f);
     printTemperatureWarning(101.9f, 95.0f, 102.0f, 102.0f * 0.015f);
     printTemperatureWarning(98.0f, 95.0f, 102.0f, 102.0f * 0.015f); 
-
-    cout << "Testing pulse warnings:\n";
+    
     printPulseWarning(60.0f, 60.0f, 100.0f, 100.0f * 0.015f);
     printPulseWarning(99.0f, 60.0f, 100.0f, 100.0f * 0.015f);
     printPulseWarning(80.0f, 60.0f, 100.0f, 100.0f * 0.015f); 
 
-    cout << "Testing spo2 warnings:\n";
     printSpo2Warning(90.0f, 90.0f, 100.0f, 100.0f * 0.015f);
     printSpo2Warning(92.0f, 90.0f, 100.0f, 100.0f * 0.015f); 
 }
-
 void testEvaluateVitals() {
     VitalSigns normal{98.6f, 75.0f, 97.0f};
     assert(evaluateVitals(normal) == VitalStatus::OK);
